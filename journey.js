b58100a88604b429087511a95c5a5064d47ddbe9
maham <mahamsajid450@gmail.com>
2025-03-04 20:54:41 +0100

    cambios los archivos

---
+++ journey.js
@@ -0,0 +1,12 @@
+document.getElementById("exploreMore").addEventListener("click", function() {
+    const extraContent = document.getElementById("extraContent");
+
+    // Toggle visibility
+    if (extraContent.style.display === "none" || extraContent.style.display === "") {
+        extraContent.style.display = "block";
+        this.innerText = "Show Less"; // Change button text
+    } else {
+        extraContent.style.display = "none";
+        this.innerText = "Explore More"; // Reset button text
+    }
+});
